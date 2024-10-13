31e7b47daf7fe436d56c1855d5efcb507c12f504
gop <gameofpointers@proton.me>
2024-10-13 14:22:36 -0500

    Updated the conversion freq to 10%

--- go.mod
+++ go.mod
@@ -17,13 +17,16 @@ require (
 )
 
 require (
+	git.sr.ht/~sbinet/gg v0.5.0 // indirect
 	github.com/BurntSushi/toml v1.3.2 // indirect
 	github.com/DataDog/zstd v1.4.5 // indirect
 	github.com/VictoriaMetrics/fastcache v1.12.2 // indirect
 	github.com/adrg/xdg v0.4.0 // indirect
+	github.com/ajstarks/svgo v0.0.0-20211024235047-1546f124cd8b // indirect
 	github.com/benbjohnson/clock v1.3.5 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/btcsuite/btcd/chaincfg/chainhash v1.1.0 // indirect
+	github.com/campoy/embedmd v1.0.0 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/cockroachdb/errors v1.8.1 // indirect
 	github.com/cockroachdb/logtags v0.0.0-20190617123548-eb05cc24525f // indirect
@@ -37,8 +40,12 @@ require (
 	github.com/dominant-strategies/bn256 v0.0.0-20220930122411-fbf930a7493d // indirect
 	github.com/edsrzf/mmap-go v1.1.0 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/go-fonts/liberation v0.3.1 // indirect
+	github.com/go-latex/latex v0.0.0-20230307184459-12ec69307ad9 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/go-pdf/fpdf v0.8.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
@@ -80,6 +87,7 @@ require (
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
 	github.com/prometheus/client_golang v1.16.0 // indirect
 	github.com/prometheus/client_model v0.4.0 // indirect
@@ -109,8 +117,10 @@ require (
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.26.0 // indirect
 	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
+	golang.org/x/image v0.11.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	gonum.org/v1/plot v0.14.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	lukechampine.com/blake3 v1.2.1 // indirect
--- transaction_test.go
+++ transaction_test.go
@@ -272,7 +272,7 @@ func TestRedeemQuai(t *testing.T) {
 	}
 	fmt.Println("Balance: ", balance)
 	fmt.Println("Nonce: ", nonce)
-	inner_tx := types.QuaiTx{ChainID: PARAMS.ChainID, Nonce: nonce, GasTipCap: MINERTIP, GasFeeCap: BASEFEE, Gas: GAS * 10, To: &toAddr, Value: common.Big0, Data: nil, AccessList: types.AccessList{}}
+	inner_tx := types.QuaiTx{ChainID: PARAMS.ChainID, Nonce: nonce, MinerTip: MINERTIP, GasPrice: BASEFEE, Gas: GAS * 10, To: &toAddr, Value: common.Big0, Data: nil, AccessList: types.AccessList{}}
 
 	tx := types.NewTx(&inner_tx)
 	tx, err = types.SignTx(tx, signer, privKey)
@@ -358,7 +358,7 @@ func TestQuaiConversion(t *testing.T) {
 	fmt.Println("Balance: ", balance)
 	fmt.Println("Nonce: ", nonce)
 
-	inner_tx := types.QuaiTx{ChainID: PARAMS.ChainID, Nonce: nonce, GasTipCap: MINERTIP, GasFeeCap: BASEFEE, Gas: GAS * 10, To: &to, Value: big.NewInt(params.Ether), Data: nil, AccessList: types.AccessList{}}
+	inner_tx := types.QuaiTx{ChainID: PARAMS.ChainID, Nonce: nonce, MinerTip: MINERTIP, GasPrice: BASEFEE, Gas: GAS * 10, To: &to, Value: big.NewInt(params.Ether), Data: nil, AccessList: types.AccessList{}}
 	tx := types.NewTx(&inner_tx)
 	tx, err = types.SignTx(tx, signer, privKey)
 	if err != nil {
@@ -383,7 +383,7 @@ func TestQuaiConversion(t *testing.T) {
 	}
 	fmt.Printf("Receipt: %+v\n", receipt)
 
-	etx := receipt.Etxs[0]
+	etx := receipt.OutboundEtxs[0]
 	tx, isPending, err = client.TransactionByHash(context.Background(), etx.Hash())
 	if err != nil {
 		fmt.Println(err.Error())
@@ -445,7 +445,7 @@ func TestETX(t *testing.T) {
 	fmt.Println("Balance: ", balance)
 	fmt.Println("Nonce: ", nonce)
 
-	inner_tx := types.QuaiTx{ChainID: PARAMS.ChainID, Nonce: nonce, GasTipCap: MINERTIP, GasFeeCap: BASEFEE, Gas: GAS * 3, To: &to, Value: VALUE, Data: nil, AccessList: types.AccessList{}}
+	inner_tx := types.QuaiTx{ChainID: PARAMS.ChainID, Nonce: nonce, MinerTip: MINERTIP, GasPrice: BASEFEE, Gas: GAS * 3, To: &to, Value: VALUE, Data: nil, AccessList: types.AccessList{}}
 	tx := types.NewTx(&inner_tx)
 
 	tx, err = types.SignTx(tx, signer, privKey)
@@ -475,7 +475,7 @@ func TestETX(t *testing.T) {
 	}
 	fmt.Printf("Receipt: %+v\n", receipt)
 
-	etx := receipt.Etxs[0]
+	etx := receipt.OutboundEtxs[0]
 	tx, isPending, err = cyprus2Client.TransactionByHash(context.Background(), etx.Hash())
 	if err != nil {
 		fmt.Println(err.Error())
@@ -532,7 +532,7 @@ func TestETXs(t *testing.T) {
 		fmt.Println("Balance: ", balance)
 		fmt.Println("Nonce: ", nonce)
 
-		inner_tx := types.QuaiTx{ChainID: PARAMS.ChainID, Nonce: nonce, GasTipCap: MINERTIP, GasFeeCap: BASEFEE, Gas: GAS * 3, To: &toAddress, Value: VALUE, Data: nil, AccessList: types.AccessList{}}
+		inner_tx := types.QuaiTx{ChainID: PARAMS.ChainID, Nonce: nonce, MinerTip: MINERTIP, GasPrice: BASEFEE, Gas: GAS * 3, To: &toAddress, Value: VALUE, Data: nil, AccessList: types.AccessList{}}
 		tx := types.NewTx(&inner_tx)
 
 		tx, err = types.SignTx(tx, signer, privKey)
@@ -610,7 +610,7 @@ func TestETXSmartContract(t *testing.T) {
 	signer := types.LatestSigner(PARAMS)
 	toAddr := common.ZeroAddress(common.Location{0, 1})
 	// Construct deployment tx
-	inner_tx := types.QuaiTx{ChainID: PARAMS.ChainID, Nonce: nonce, GasTipCap: MINERTIP, GasFeeCap: MAXFEE, Gas: 5000000, To: &toAddr, Value: common.Big0, Data: contract}
+	inner_tx := types.QuaiTx{ChainID: PARAMS.ChainID, Nonce: nonce, MinerTip: MINERTIP, GasPrice: MAXFEE, Gas: 5000000, To: &toAddr, Value: common.Big0, Data: contract}
 	tx, err := types.SignTx(types.NewTx(&inner_tx), signer, privKey)
 	if err != nil {
 		fmt.Println(err.Error())
@@ -637,7 +637,7 @@ func TestETXSmartContract(t *testing.T) {
 	}
 	fmt.Printf("Receipt: %+v\n", receipt)
 
-	etx := receipt.Etxs[0]
+	etx := receipt.OutboundEtxs[0]
 	tx, isPending, err = cyprus2Client.TransactionByHash(context.Background(), etx.Hash())
 	if err != nil {
 		fmt.Println(err.Error())
@@ -701,11 +701,11 @@ func TestSmartContract(t *testing.T) {
 	fmt.Printf("Balance of %s: %s nonce: %d\n", fromAddress.String(), balance.String(), nonce)
 
 	// Deploy QXC contract with the proper address that gives me tokens in zone 0-0
-	contract, err := hex.DecodeString(binary)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
+	// contract, err := hex.DecodeString(binary)
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// 	return
+	// }
 	/*i := uint8(0)
 		j := uint8(0)
 		contract = append(contract, i)
@@ -728,18 +728,35 @@ func TestSmartContract(t *testing.T) {
 	fmt.Println("Took ", (i+1)*(j+1), " iterations to find contract address")*/
 	signer := types.LatestSigner(PARAMS)
 	// Construct deployment tx
-	inner_tx := types.QuaiTx{ChainID: PARAMS.ChainID, Nonce: nonce, GasTipCap: MINERTIP, GasFeeCap: MAXFEE, Gas: 5000000, To: nil, Value: common.Big0, Data: contract}
-	tx, err := types.SignTx(types.NewTx(&inner_tx), signer, privKey)
+
+	robert_contract, _ := hex.DecodeString("608060405234801561001057600080fd5b5061058d806100206000396000f3fe608060405234801561001057600080fd5b50600436106100675760003560e01c8063c8d6fda711610050578063c8d6fda7146100a4578063db734248146100b9578063f24684e5146100cc57600080fd5b80633da613681461006c578063b206699b14610091575b600080fd5b61007f61007a3660046102aa565b6100df565b60405190815260200160405180910390f35b61007f61009f366004610321565b61013a565b6100b76100b2366004610374565b610167565b005b61007f6100c7366004610419565b61024d565b61007f6100da366004610473565b610295565b6000816000036101365760405162461bcd60e51b815260206004820152601360248201527f50616e69633a20636f6465206973207a65726f0000000000000000000000000060448201526064015b60405180910390fd5b5090565b600082828561015d5760405162461bcd60e51b815260040161012d9291906104be565b5091949350505050565b60405186907f85c55bbb820e6d71c71f4894e57751de334b38c421f9c170b0e66d32eafea33790600090a260405173ffffffffffffffffffffffffffffffffffffffff8616907f52cb491081609a3d8c50cb8d5c1395de748f65789fc66e140e795decadd86c3090600090a27f7240e2f75cccc64acf37f699b7cc2726ccd9c0ed8afeafdbf7911af78d077bad84846040516102049291906104be565b60405180910390a17f06e852ba9138ee18ce13f482908b8634bc29d809282ea568cf505aca412b195e828260405161023d9291906104be565b60405180910390a1505050505050565b60008461028c578383836040517f180c751a00000000000000000000000000000000000000000000000000000000815260040161012d939291906104da565b50919392505050565b60006102a182846104fd565b90505b92915050565b6000602082840312156102bc57600080fd5b5035919050565b803580151581146102d357600080fd5b919050565b60008083601f8401126102ea57600080fd5b50813567ffffffffffffffff81111561030257600080fd5b60208301915083602082850101111561031a57600080fd5b9250929050565b60008060006040848603121561033657600080fd5b61033f846102c3565b9250602084013567ffffffffffffffff81111561035b57600080fd5b610367868287016102d8565b9497909650939450505050565b6000806000806000806080878903121561038d57600080fd5b86359550602087013573ffffffffffffffffffffffffffffffffffffffff811681146103b857600080fd5b9450604087013567ffffffffffffffff808211156103d557600080fd5b6103e18a838b016102d8565b909650945060608901359150808211156103fa57600080fd5b5061040789828a016102d8565b979a9699509497509295939492505050565b6000806000806060858703121561042f57600080fd5b610438856102c3565b935060208501359250604085013567ffffffffffffffff81111561045b57600080fd5b610467878288016102d8565b95989497509550505050565b6000806040838503121561048657600080fd5b50508035926020909101359150565b81835281816020850137506000828201602090810191909152601f909101601f19169091010190565b6020815260006104d2602083018486610495565b949350505050565b8381526040602082015260006104f4604083018486610495565b95945050505050565b808201808211156102a4577f4e487b7100000000000000000000000000000000000000000000000000000000600052601160045260246000fdfea2646970667358221220aa52c22ce2f4a856b56d32ae58c6b41a9dba4c7aed43a0872ffa898f157e67b564736f6c637822302e382e31392d646576656c6f702b636f6d6d69742e63383866343066642e6d6f640053")
+
+	inner_tx := types.QuaiTx{ChainID: PARAMS.ChainID, Nonce: nonce, GasPrice: MINERTIP, MinerTip: MAXFEE, Gas: 5000000, To: nil, Value: common.Big0, Data: robert_contract}
+
+	// get access list
+	accessList, err := client.CreateAccessList(context.Background(), interfaces.CallMsg{From: fromAddress, To: nil, Gas: 4000000, GasPrice: MINERTIP, MinerTip: MINERTIP, Value: common.Big0, Data: robert_contract})
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
-	gas, err := client.EstimateGas(context.Background(), interfaces.CallMsg{From: fromAddress /*To: nil, Gas: 0, GasPrice: MAXFEE, GasFeeCap: MAXFEE, GasTipCap: MINERTIP, Value: common.Big0, */, Data: contract, AccessList: inner_tx.AccessList})
+
+	inner_tx.AccessList = append(inner_tx.AccessList, *accessList.Accesslist...)
+
+	gas, err := client.EstimateGas(context.Background(), interfaces.CallMsg{From: fromAddress, GasPrice: MAXFEE, MinerTip: MAXFEE /*To: nil, Gas: 0, GasPrice: MAXFEE, GasFeeCap: MAXFEE, GasTipCap: MINERTIP, Value: common.Big0, */, Data: robert_contract, AccessList: inner_tx.AccessList})
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
 	fmt.Println("gas: ", gas)
+
+	inner_tx.Gas = gas
+
+	tx, err := types.SignTx(types.NewTx(&inner_tx), signer, privKey)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
 	/*accessList, err := client.CreateAccessList(context.Background(), interfaces.CallMsg{From: fromAddress, Data: contract})
 	if err != nil {
 		fmt.Println(err.Error())
